57ede4af06fc2a7782a6ed904952b92cb4f08a95
安歌 <qdwgdut@126.com>
2021-06-13 15:03:48 +0800

    feat: base usage about vue@next

--- vue-next-typescript/src/App.vue
+++ vue-next-typescript/src/App.vue
@@ -1,22 +1,36 @@
 <template>
   <div class="app">
     <h3 class="title">{{ title }}</h3>
-    <p class="author">{{ author }}</p>
+    <p class="author">author: {{ author }} / age: {{ age }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'App',
   components: {},
   setup() {
-    const data = reactive({
-      title: 'Film Site',
-      author: 'wilton'
-    })
-    return { ...toRefs(data) }
+    // const state = reactive({
+    //   title: 'Film Site' as string,
+    //   author: 'wilton' as string,
+    //   age: 18 as number | string
+    // })
+    // setTimeout(() => {
+    //   state.age = 20
+    //   console.log(state)
+    // }, 1000);
+    // return { ...toRefs(state) }
+
+    const title = ref<string>('Film Site')
+    const author = ref('wilton')
+    const age = ref<number | string>(18)
+
+    setTimeout(() => {
+      age.value = '20'
+    }, 1000);
+    return { title, author, age }
   }
 });
 </script>
