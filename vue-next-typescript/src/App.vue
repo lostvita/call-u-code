382041794702a05cc2270ce1762ce9c106167af2
安歌 <qdwgdut@126.com>
2021-06-13 14:09:05 +0800

    feat: init project

---
+++ vue-next-typescript/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app">
+    <h3 class="title">{{ title }}</h3>
+    <p class="author">{{ author }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+  setup() {
+    const data = reactive({
+      title: 'Film Site',
+      author: 'wilton'
+    })
+    return { ...toRefs(data) }
+  }
+});
+</script>
+
+<style lang="less" scoped>
+.app {
+  color: tomato;
+}
+</style>
