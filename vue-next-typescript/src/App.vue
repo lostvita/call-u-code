129bbaaf51e6842308479ee81b242b2096426feb
安歌 <qdwgdut@126.com>
2021-06-19 12:53:49 +0800

    feat: computed & function & v-model

--- vue-next-typescript/src/App.vue
+++ vue-next-typescript/src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="app">
     <h3 class="title">{{ title }}</h3>
-    <film-list :films="filmList"></film-list>
+    <search-bar v-model="keyword"></search-bar>
+    <order-by v-model="order"></order-by>
+    <film-list :films="filmList" :order="order" :search="keyword"></film-list>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import FilmList from './components/film-list.vue';
+import OrderBy from './components/order-by.vue';
+import SearchBar from './components/search-bar.vue';
 import { FilmItem } from './types/item'
+import { OrderTerm } from './types/term'
 
 export default defineComponent({
   name: 'App',
-  components: {FilmList},
+  components: {FilmList, OrderBy, SearchBar},
   setup() {
     const title = ref<string>('Film Site@Call U Code')
     const filmList = ref<FilmItem[]>([
@@ -28,12 +33,16 @@ export default defineComponent({
         id: 2,
         title: '投敌者',
         picture: 'https://static-movie.a.88cdn.com/d1d8c14f4fb0f6e402f9a60e85fea98c.jpg?imageView2/1/w/400/h/560/q/90/interlace/1/format/webp',
-        category: '剧情',
-        region: '美国',
+        category: '历史',
+        region: '德国',
         description: '在危机四伏的投资界，掌管史崔顿·奥克蒙公司的乔丹·贝尔福特（莱昂纳多·迪卡普里奥 Leonardo DiCaprio 饰）生活侈靡、实力雄厚。1987年，22岁的乔丹进入罗斯柴尔德公司，从接线员做起，在高级经纪人马克（马修·麦康纳 Matthew McConaughey 饰）指点下，进入了股票经纪人充满毒品和欲望的世界，半年后因“黑色星期一”，乔丹转投靠贩卖“便士股票”的小公司，凭借巧舌如簧获得成功，与邻居唐尼（乔纳·希尔 Jonah Hill 饰）和一班朋友另立门户，游走在灰色地带获利颇丰，获称“华尔街之狼”。'
       }
     ])
-    return { title, filmList }
+    
+    const keyword = ref('')
+    const order = ref<OrderTerm>('title')
+    
+    return { title, filmList, keyword, order }
   }
 });
 </script>
@@ -43,6 +52,8 @@ export default defineComponent({
   width: 100%;
 }
 .title {
+  padding: 20px 0;
+  margin: 20px auto;
   text-align: center;
   color: peru;
 }
---
+++ vue-next-typescript/src/components/search-bar.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="search-wrapper">
+    <div class="search-group"><input type="text" name="" placeholder="搜索片名" v-model="keyword"></div>
+    <div class="seatch-controls"><button class="search-btn" @click="searchHandle">搜索</button></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'SearchBar',
+  props: {
+    modelValue: {
+      type: String,
+      default: ''
+    }
+  },
+  setup(props, { emit }) {
+    const keyword = ref('')
+    const searchHandle = () => {
+      if (!keyword.value) return
+      emit('update:modelValue', keyword.value)
+    }
+    return { keyword, searchHandle }
+  },
+})
+</script>
+
+<style lang="less" scoped>
+.search-wrapper {
+  max-width: 960px;
+  height: auto;;
+  margin: 20px auto;
+  display: flex;
+}
+.search-group {
+  flex-grow: 1;
+  padding: 0 10px;
+  background-color: #fff;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  & > input {
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    border: none;
+    outline: none;
+    background: transparent;
+  }
+}
+.search-controls {
+  width: 120px;
+}
+.search-btn {
+  --lightness: 92%;
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: hsl(201, 100%, var(--lightness));
+  padding: 5px 15px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+  &:hover {
+    --lightness: 82%;
+  }
+}
+</style>
