25d08bb3e22b957c7fb903a3ecb2f99fdf765a6f
安歌 <qdwgdut@126.com>
2021-06-27 18:22:24 +0800

    feat: complete calculate logic

--- calculator-javascript/script.js
+++ calculator-javascript/script.js
@@ -2,20 +2,85 @@ class Calculator {
   constructor(previousTextEle, currentTextEle) {
     this.previousTextEle = previousTextEle
     this.currentTextEle = currentTextEle
+    this.equalsLock = false
     this.clear()
   }
 
-  clear() {}
+  setEqualsLock(flag) {
+    if (this.equalsLock === flag) return
+    this.equalsLock = flag
+  }
 
-  delete() {}
+  clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = null
+  }
 
-  appendNumber(number) {}
+  delete() {
+    if (this.equalsLock) return
+    this.currentOperand = this.currentOperand.slice(0, -1)
+  }
 
-  chooseOperation(operation) {}
+  appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return
+    this.equalsLock && (this.currentOperand = '')
+    this.currentOperand += number.toString()
+    calculator.setEqualsLock(false)
+  }
 
-  compute() {}
+  chooseOperation(operation) {
+    if (this.currentOperand === '') return
+    if (this.previousOperand !== '') {
+      this.compute()
+    }
+    this.operation = operation
+    this.previousOperand = this.currentOperand
+    this.currentOperand = ''
+  }
 
-  updateDisplay() {}
+  compute() {
+    let computation
+    const prev = parseFloat(this.previousOperand)
+    const curr = parseFloat(this.currentOperand)
+    if (isNaN(prev) || isNaN(curr)) return
+    switch (this.operation) {
+      case '+':
+        computation = prev + curr
+        break
+      case '-':
+        computation = prev - curr
+        break
+      case '*':
+        computation = prev * curr
+        break
+      case '÷':
+        computation = prev / curr
+        break
+      default:
+        return
+    }
+    this.currentOperand = computation.toString()
+    this.previousOperand = ''
+    this.operation = null
+  }
+
+  numberFormat(number) {
+    const numbers = number.toString().split('.')
+    const integer = parseInt(numbers[0])
+    const decimal = numbers[1]
+    let integerDisplay = isNaN(integer) ? '' : integer.toLocaleString()
+    return decimal != null ? `${integerDisplay}.${decimal}` : integerDisplay
+  }
+
+  updateDisplay() {
+    this.currentTextEle.innerText = this.numberFormat(this.currentOperand)
+    if (this.operation) {
+      this.previousTextEle.innerText = `${this.numberFormat(this.previousOperand)} ${this.operation}`
+    } else {
+      this.previousTextEle.innerText = ''
+    }
+  }
 }
 
 const previousTextEle = document.querySelector('[data-previous-operand]')
@@ -37,12 +102,14 @@ numberBtns.forEach(btn => {
 
 operationBtns.forEach(btn => {
   btn.addEventListener('click', () => {
+    calculator.setEqualsLock(false)
     calculator.chooseOperation(btn.innerText)
     calculator.updateDisplay()
   })
 })
 
 allClearBtn.addEventListener('click', () => {
+  calculator.setEqualsLock(false)
   calculator.clear()
   calculator.updateDisplay()
 })
@@ -53,6 +120,7 @@ deleteBtn.addEventListener('click', () => {
 })
 
 equalsBtn.addEventListener('click', () => {
+  calculator.setEqualsLock(true)
   calculator.compute()
   calculator.updateDisplay()
 })
